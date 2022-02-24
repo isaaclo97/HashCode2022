5b57c5fbf003f4d73cca78069cc076d69d6d9360
Isaac Lozano Osorio <Isaac.lozano.osorio.97@gmail.com>
2022-02-24 22:10:10 +0100

    LS

--- contest/src/main/java/es/urjc/etsii/grafo/HashCode/improve/XXLocalSearch.java
+++ contest/src/main/java/es/urjc/etsii/grafo/HashCode/improve/XXLocalSearch.java
@@ -3,10 +3,12 @@ package es.urjc.etsii.grafo.HashCode.improve;
 import es.urjc.etsii.grafo.HashCode.io.SolutionImprovedEvent;
 import es.urjc.etsii.grafo.HashCode.model.HashCodeInstance;
 import es.urjc.etsii.grafo.HashCode.model.HashCodeSolution;
+import es.urjc.etsii.grafo.HashCode.model.Person;
+import es.urjc.etsii.grafo.HashCode.model.Skill;
 import es.urjc.etsii.grafo.solver.improve.Improver;
 import es.urjc.etsii.grafo.solver.services.events.EventPublisher;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class XXLocalSearch extends Improver<HashCodeSolution, HashCodeInstance> {
 
@@ -17,8 +19,34 @@ public class XXLocalSearch extends Improver<HashCodeSolution, HashCodeInstance> 
 
         while(improved) {
             improved = false;
-            throw new UnsupportedOperationException("XXLocalSearch::improve not implemented yet");
+            for(var p: hashCodeSolution.getProjectOrder()){
+                Map<Skill, Person> setValue = new HashMap<>(hashCodeSolution.getAssignments().get(p));
+                Map<String, Person> getPersons = new HashMap<>(hashCodeSolution.getInstance().getPersons());
+                for(var e: setValue.keySet()){
+                    for(var people: getPersons.values()) {
+                        String skillAsigned = e.getName();
+                        int levelAssigned = e.getN();
+                        Person lastUser =  hashCodeSolution.getAssignments().get(p).get(skillAsigned);
+                        if(people.getSkills().getOrDefault(skillAsigned,0) >= levelAssigned){
+                            hashCodeSolution.getAssignments().get(p).put(e,people);
+                        }
+                        else{
+                            continue;
+                        }
+                        if (currentScore  < hashCodeSolution.getScore()) {
+                            currentScore = hashCodeSolution.getScore();
+                            System.out.println(hashCodeSolution.getScore());
+                            improved = true;
+                            break;
+                        }
+                        hashCodeSolution.getAssignments().get(p).put(e,lastUser);
+                    }
+                    if(improved)
+                        break;
+                }
+            }
         }
+        //System.out.println(hashCodeSolution.getScore());
         return hashCodeSolution;
     }
 
